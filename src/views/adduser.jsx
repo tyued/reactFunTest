96f8b0922b893a368327b232a9a9b1c30221fb27
rayjcr <rayj@qq.com>
2021-12-07 13:52:33 +0800

    添加了Zoom 动态效果

--- src/views/adduser.jsx
+++ src/views/adduser.jsx
@@ -1,15 +1,34 @@
-import { Container, TextField, Box, Select, MenuItem, InputLabel, FormControl } from '@material-ui/core'
-import React, { useState,useEffect } from 'react'
+import { Container, TextField, Box, Select, MenuItem, InputLabel, FormControl, FormControlLabel, Switch, Zoom } from '@material-ui/core';
+import React, { useState,useEffect } from 'react';
+import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid'
+import { useDemoData } from '@mui/x-data-grid-generator'
 import { useHistory } from 'react-router';
+
+function CustomToolbar(){
+    return (
+        <GridToolbarContainer>
+            <GridToolbarExport/>
+        </GridToolbarContainer>
+    );
+}
 
 export default function Adduser({location}) {
     const [formData, setFormData] = useState({});
+    const [checked, setChecked] = useState(false);
+
+
     console.log(location,'adduser-page-location')
     let history = useHistory();
 
+    const { data } = useDemoData({
+        dataSet: 'Commodity',
+        rowLength: 10,
+        maxColumns: 6,
+    })
+
+
     useEffect(() => {
-        console.log(history,'use以后的history')
-        console.log("我这里进来了")
+        console.log("我这里进来useEffect")
     });
 
     const handleChange = (propName) => {
@@ -17,7 +36,8 @@ export default function Adduser({location}) {
             setFormData({...formData,[propName]:event.target.value})
         }
     }
-    console.log(formData,'formData--a')
+    
+    console.log(data,'data--a')
     return (
         <div>
             这里是用户添加的表单页面
@@ -43,8 +63,22 @@ export default function Adduser({location}) {
                         </Select>
                     </FormControl>
                 </Box>
-                <Box style={{padding:'10px 0'}}>
-
+                <FormControlLabel 
+                    control={<Switch checked={checked}
+                    onChange={() => {
+                        setChecked((prev) => !prev)
+                    }} />}
+                    label='Show'></FormControlLabel>
+                
+                <Box style={{ height:'300px',width:'100%',padding:'10px 0'}}>
+                    <Zoom in={checked}>
+                        <DataGrid {...data} components={{Toolbar:CustomToolbar,}}>
+                        </DataGrid>
+                    </Zoom>
+                    <Zoom in={checked} style={{ transitionDelay: checked ? '500ms' : '0ms' }}>
+                        <DataGrid {...data} components={{Toolbar:CustomToolbar,}}>
+                        </DataGrid>
+                    </Zoom>
                 </Box>
             </Container>
         </div>
